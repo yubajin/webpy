3625cb699fcb399cd83838ec76dc0a0e2ffa7b4c
yubajin <1803279085@qq.com>
2017-09-24 16:52:53 +0800

    init commit with a web demo return a helloworld and a demo return a simple html

--- com/yubajin/Mywebpy/web_html.py
+++ com/yubajin/Mywebpy/web_html.py
@@ -1,15 +1,37 @@
 
 import web
 
+# url映射
 urls = (
+    '/index','index',
+    '/blog/\d+','blog',
     '/(.*)','hello'
 )
 
 app = web.application(urls, globals())
 
+class index:
+# 请求路径比如http://localhost:8080/index?name=yubajin&sex=female
+    def GET(self):
+        data = web.input()
+        return data
+
+class blog:
+    # def GET(self):
+    #     data = web.input()
+    #     return data
+
+    def GET(self):
+            return web.ctx.env
+
+    def POST(self):
+        data = web.input()
+        return data
+
+# 请求路径比如http://localhost:8080
 class hello:
     def GET(self,name):
-        result = open(r'htmlfile/1.html','r').read()
+        result = open(r'htmlfile/2.html','r').read()
         return result
 
 if __name__ == '__main__':
